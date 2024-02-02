e96a1c8547645d7b0ff02fd7f4263e11342830ba
Matheus Gabriel <matheusgabriel.d.barros@gmail.com>
2024-02-02 17:23:05 -0300

    create menu bar and footbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { FootbarComponent } from './components/footbar/footbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    MenuComponent,
+    FootbarComponent
   ],
   imports: [
     BrowserModule,
